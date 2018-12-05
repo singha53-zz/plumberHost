e4aeefb257e341b46522de5fb7433f4efd27145e
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-12-05 12:14:40 -0500

    change project

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express');
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-const app = express();
+const getDate = require('./date.js');
 
-app.get('/', (req, res) => {
-  res.send('Hi there');
+let server = http.createServer((req, res) => {
+	let html = `<h1>Environment Variables</h1>`;
+	// Add new code to test the live reload
+	html += `<h2>As of: ${getDate()}</h2><br>`;
+	// Iterate over `process.env` object and
+	// print its values.
+	Object.keys(process.env).forEach((k) => {
+		html += `${k} = ${process.env[k]} <br>`
+	});
+	// Set the response status and response content 
+	// type header
+	res.writeHead(200, {
+		'content-type': 'text/html'
+	});
+	return res.end(html);
 });
 
-app.listen(8080, () => {
-  console.log('listening on port 8080');
-})
+// start listening
+server.listen(port);
+console.log(`Server running on port ${port}`);
