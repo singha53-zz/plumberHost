4105933c5aaa4b162c20bb58df1ed8b5b14dda65
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-12-05 12:19:36 -0500

    add express app

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-const http = require('http');
 const port = process.env.PORT || 3000;
+const express = require('express');
 
-const getDate = require('./date.js');
+const app = express();
 
-let server = http.createServer((req, res) => {
-	let html = `<h1>Environment Variables</h1>`;
-	// Add new code to test the live reload
-	html += `<h2>As of: ${getDate()}</h2><br>`;
-	// Iterate over `process.env` object and
-	// print its values.
-	Object.keys(process.env).forEach((k) => {
-		html += `${k} = ${process.env[k]} <br>`
-	});
-	// Set the response status and response content 
-	// type header
-	res.writeHead(200, {
-		'content-type': 'text/html'
-	});
-	return res.end(html);
+app.get('/', (req, res) => {
+  res.send('Hi there');
 });
 
-// start listening
-server.listen(port);
-console.log(`Server running on port ${port}`);
+app.listen(port, () => {
+  console.log(`listening on port${port}`);
+})
